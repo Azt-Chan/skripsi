c10c59c739d59ab4bf9590ec6c2532f6d7ea9543
Azt-Chan <astutihadijahpili@gmail.com>
2024-05-28 21:51:49 +0700

    fix: connection

--- koneksi.php
+++ koneksi.php
@@ -4,7 +4,7 @@ require_once "parser-php-version.php";
 
 	$host="localhost";
 	$user="root";
-	$password="root";
+	$password="";
 	$database="darulfalah";
 	@$koneksi=mysql_connect($host,$user,$password);
 	mysql_select_db($database,$koneksi);
